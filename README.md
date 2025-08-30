9bc422913338f0472b9251562fa21ee85bdc9315
Amazing Cabiles <amazinggracecabiles@gmail.com>
2025-08-30 23:00:30 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Intermediate Python Programming
+https://youtu.be/HGOBQPFzWKo?si=r0C8djSYc8Weri7i
---
+++ lists.py
@@ -0,0 +1,49 @@
+mylist = ["banana", "cherry", "apple"]
+# print(mylist)
+
+# create an empty list to later append items onto
+# mylist2 = list()
+# print(mylist2)
+
+# List with other data types
+# mylist2 = [5, True, "apple", "apple"]
+# print(mylist2)
+
+# Access an element inside the array/list
+# item = mylist[0]
+# print(item)
+
+# Get the last item of the list using -1
+# Change to -2 to get the second last item from the list
+# item = mylist[-1]
+# print(item)
+
+# Iterate through the list and print all elements inside
+# for i in mylist:
+#     print(i)
+
+# Check if item is inside the list
+# if "banana" in mylist:
+#     print("yes")
+# else:
+#     print("no")
+
+# Check how many elements inside the list
+# print(len(mylist))
+
+# Append items to the list
+# mylist.append("lemon")
+# print(mylist)
+
+# Insert item at a specific position
+# mylist.insert(1, "blueberry")
+# print(mylist)
+
+# Remove items inside the list
+# Returns the last item and also removes it
+# item = mylist.pop()
+# print(item)
+# print(mylist)
+
+# Remove a specific element
+# https://youtu.be/HGOBQPFzWKo?si=N5SyTdzlu17FdsHo&t=426
