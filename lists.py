586b72595dcf8d7a243ace2c347b2df25c604a4f
Amazing Cabiles <amazinggracecabiles@gmail.com>
2025-09-21 20:27:26 +0800

    9/21/2025

--- lists.py
+++ lists.py
@@ -1,4 +1,4 @@
-mylist = ["banana", "cherry", "apple"]
+# mylist = ["banana", "cherry", "apple"]
 # print(mylist)
 
 # create an empty list to later append items onto
@@ -46,4 +46,81 @@ mylist = ["banana", "cherry", "apple"]
 # print(mylist)
 
 # Remove a specific element
-# https://youtu.be/HGOBQPFzWKo?si=N5SyTdzlu17FdsHo&t=426
+# item = mylist.remove("cherry")
+# print(mylist) 
+
+# Remove all elements using Clear
+# item = mylist.clear()
+# print(mylist)
+
+# Reverse the list
+# item = mylist.reverse()
+# print(mylist)
+
+# Sort the list
+# item = mylist.sort()
+# print(mylist)
+
+# Sort list of numbers
+# mylist = [4, 3, 1, -1, -5, 10]
+# item = mylist.sort()
+# print(mylist)
+
+# Create list with 5 zeros
+# mylist = [0] * 5
+# print(mylist)
+
+# Add two lists together
+# mylist = [0] * 5
+# mylist2 = [1, 2, 3, 4, 5]
+# new_list = mylist + mylist2
+# print(new_list)
+
+# Slicing - access some parts of the list
+# mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# specify start and stop index which in this case is 1 and 5
+# the last index is excluded so the list is [2, 3, 4, 5]
+# a = mylist[1:5]
+# if no start index is specified then it starts from the beginning
+# if no stop index is specified then it goes all the way to the end
+# print(a)
+
+# Slicing with Step index (optional)
+# mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Default Step index is 1
+# This goes all the way from the beginning to the end with a step index of 1
+# a = mylist[::1]
+
+# If step index is 2 then it takes every second item
+# a = mylist[::2]
+
+# A negative index just reverses the list
+# a = mylist[::-1]
+# print(a)
+
+# Copying a list
+list_orig = ["banan", "cherry", "apple"]
+# Copies the contents of the original list and allows modification without changing the original list
+# list_copy = list_orig.copy()
+# list_copy.append("lemon")
+
+# Another way to do this is instead of copy just use list(original_list)
+# list_copy = list(list_orig)
+# list_copy.append("lemon")
+
+# Slicing can also be used to create a copy of the original list
+# list_copy = list_orig[:]
+# list_copy.append("lemon")
+
+# list_copy shows the lemon but the original list stays the same
+# print(list_copy)
+# print(list_orig)
+
+# List Comprehension
+# Create new list from an existing list all in one line
+mylist = [1, 2, 3, 4, 5, 6]
+# create list of squared numbers
+b = [i*i for i in mylist]
+print(mylist)
+# This shows the newly created list of squared numbers based off of mylist
+print(b)
